bc0c03cbc21d8bd5566ec4a3aada4932bd066c00
Thiago Boa <thiago@23-milhas.local>
2019-04-20 19:13:30 -0300

    First hero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="stage">
+          <div className="stage--block stage--block__black">
+            <h1 className="hero hero__black">thiago</h1>
+            <h1 className="hero hero__black">boa</h1>
+          </div>
+          <div className="stage--block stage--block__white">
+            <h2 className="hero hero__white">design</h2>
+            <h2 className="hero hero__white">technologist</h2>
+          </div>
+        </div>
       </div>
     );
   }
